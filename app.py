64f3cc8af50a3a060a7480e4d5299aeb5f4d0ad5
ppi1u16 <ppi1u16@ecs.soton.ac.uk>
2019-06-11 21:49:56 +0100

    fixbug

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    output = prediction
 
     return render_template('ml.html', prediction_text='The predicted employee salary is $ {}'.format(output))
 
